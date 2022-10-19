ed74bb98fb9e53acad69a9a953a2974afa3235e5
mrbln <belen.emre@gmail.com>
2022-10-19 17:06:48 +0200

    adds httpClient to fetch shipping prices from file

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 import { Product } from "./products";
 
 @Injectable({
-  providedIn: "root",
+    providedIn: "root",
 })
 export class CartService {
-  items: Product[] = [];
-  constructor() {}
+    items: Product[] = [];
+    constructor(private httpClient: HttpClient) {}
 
-  addItems(product:Product) {
-    this.items.push(product);
-  }
+    addItems(product: Product) {
+        this.items.push(product);
+    }
 
-  getItems() {
-    return this.items;
-  }
+    getItems() {
+        return this.items;
+    }
 
-  clearItems() {
-    this.items = [];
-  }
+    clearItems() {
+        this.items = [];
+    }
+
+    getShippingPrices() {
+        return this.httpClient.get<{ type: string; price: number }[]>(
+            "../assets/shipping.json"
+        );
+    }
 }
